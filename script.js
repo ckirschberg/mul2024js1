166415bed59b3f5db498902950591fd32b548046
Christian Kirschberg <ckirschberg@gmail.com>
2024-09-18 14:38:25 +0200

    Number conversion not needed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById("calculate").addEventListener("click", () => {
     const distance = document.getElementById("distance").value;
     const time = document.getElementById("time").value;
-    console.log(distance)
-    console.log(time)
+    // console.log(distance)
+    // console.log(time)
 
-    const speed = Number(distance) / (Number(time) / 60);
+    const speed = distance / (time / 60);
 
     document.getElementById("output").innerHTML = 'Din hastighed er ' + speed + ' km/t';
 })
