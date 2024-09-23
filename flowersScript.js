fef4434cf336b39ecd63f725dc0391ffda2f59ad
Christian Kirschberg <ckirschberg@gmail.com>
2024-09-23 14:37:06 +0200

    html template

---
+++ flowersScript.js
@@ -0,0 +1,15 @@
+const flowers = [
+    {
+        img: 'https://img.freepik.com/free-photo/front-view-female-hand-holding-bouquet-flowers_23-2149024601.jpg?t=st=1726730511~exp=1726734111~hmac=374f9be87ec2612ea02a7e43838cf23d8ceeebc26599e3e423ec911af66025d7&w=900',
+        price: 250
+    },
+    {
+        img: 'https://img.freepik.com/free-photo/top-view-beautiful-roses-bouquet-with-pink-ribbon_23-2148822309.jpg?t=st=1726730557~exp=1726734157~hmac=b142c07fc8189020467be856496d2874e542329ca24e45431b83513f96ad6398&w=1060',
+        price: 150
+    },
+    {
+        img: 'https://img.freepik.com/free-photo/top-view-beautiful-flowers-bouquet_23-2149024538.jpg?t=st=1726730579~exp=1726734179~hmac=144338628026233e55683f92218e7bb1e583f2d8c93e3ebdb7637c7c64f189f5&w=900',
+        price: 500
+    }
+];
+
--- script.js
+++ script.js
@@ -8,3 +8,65 @@ document.getElementById("calculate").addEventListener("click", () => {
 
     document.getElementById("output").innerHTML = 'Din hastighed er ' + speed + ' km/t';
 })
+
+const name = 'Philip';
+const anotherName = "Sofie"
+const myAge = 45.65;
+const isOfLegalAge = true;
+if (myAge > 50) {
+    alert("YES")
+}
+
+//let = variabel
+//const = constant
+
+ //object { } - js gør det til et objekt
+const student = { 
+    name: 'Philip', 
+    email: 'philip@world.dk', 
+    hairColor: 'Green' ,
+    age: 45.65
+}
+const student2 = {
+    name: 'Aiden',
+    email: 'aiden@world.dk',
+    hairColor: 'Black w. green',
+    age: 25
+}
+const students = [student, student2];
+
+
+student.shoeColor = 'Red';
+console.log(student);
+
+// array i javascript defineres med [] 
+const names = ["Aiden", "Sofie", "Philip", "Dillan"];
+names.push("Sinan");
+console.log(names);
+console.log(names[4])
+
+const cats = [
+    { name: "Charlie", color: "Brown"},
+    { name: "Monty", color: "White"}
+]
+
+
+
+students.forEach((student) => {
+
+    let temp = document.getElementById("studentTemplate");
+    let clon = temp.content.cloneNode(true);
+    clon.getElementById("name").innerHTML = student.name;
+    clon.getElementById("email").innerHTML = student.email;
+    clon.getElementById("hairColor").innerHTML = student.hairColor;
+    // clon.getElementById("age").innerHTML = student.age;
+
+    clon.getElementById("btnAge").addEventListener("click", () => {
+        alert("Hello from " + student.name + " my age is " + student.age );
+    })
+
+    document.getElementById("output").appendChild(clon);
+
+
+
+})
